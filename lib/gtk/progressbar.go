e2d1db8e59ea971200d68a0c31f5ea86e6ebbe6e
jopbrown <msshane2008@gmail.com>
2019-08-03 15:11:49 +0800

    add GtkProgressBar API

--- lib/gtk/progressbar.go
+++ lib/gtk/progressbar.go
@@ -1,9 +1,39 @@
 package gtk
 
+import sugar "github.com/jopbrown/gtk-sugar"
+
+type ProgressBar struct {
+	Widget
+}
+
+func NewProgressBarFromCandy(candy sugar.Candy, id string) *ProgressBar {
+	obj := ProgressBar{}
+	obj.CandyWrapper = candy.NewWrapper(id)
+	return &obj
+}
+
 // FUNCTION_NAME = gtk_progress_bar_new, NONE, WIDGET, 0
+func NewProgressBar() *ProgressBar {
+	id := Candy().Guify("gtk_progress_bar_new").String()
+	return NewProgressBarFromCandy(Candy(), id)
+}
+
 // FUNCTION_NAME = gtk_progress_bar_set_text, NONE, NONE, 2, WIDGET, STRING
+func (obj *ProgressBar) SetText(text string) {
+	obj.Candy().Guify("gtk_progress_bar_set_text", obj, text)
+}
+
 // FUNCTION_NAME = gtk_progress_bar_set_fraction, NONE, NONE, 2, WIDGET, DOUBLE
+func (obj *ProgressBar) SetFraction(fraction float64) {
+	obj.Candy().Guify("gtk_progress_bar_set_fraction", obj, fraction)
+}
+
 // FUNCTION_NAME = gtk_progress_bar_pulse, NONE, NONE, 1, WIDGET
+func (obj *ProgressBar) Pulse() {
+	obj.Candy().Guify("gtk_progress_bar_pulse", obj)
+}
+
 // FUNCTION_NAME = gtk_progress_bar_set_pulse_step, NONE, NONE, 2, WIDGET, DOUBLE
-// FUNCTION_NAME = gtk_progress_configure, NONE, NONE, 4, WIDGET, DOUBLE, DOUBLE, DOUBLE
-// FUNCTION_NAME = gtk_progress_set_value, NONE, NONE, 2, WIDGET, DOUBLE
+func (obj *ProgressBar) SetPulseStep(fraction float64) {
+	obj.Candy().Guify("gtk_progress_bar_set_pulse_step", obj, fraction)
+}
